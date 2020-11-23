1a8b97f768141e1d5536c2ceccf36511ccf995c1
Alec Scully <ScullyAlec@gmail.com>
2020-11-22 19:47:51 -0500

    commit tree 6582b7471bfc1586c7fcbe858e8c12202b0bfa7e

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,37 @@
-class BankAccount
+require 'pry'
 
+class BankAccount
+    attr_accessor :balance, :status
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = 'open'
+    end
+
+    def deposit(amount)
+        self.balance += amount
+    end
+
+    def display_balance 
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        if self.status == 'open'
+            if self.balance > 0
+                true
+            else
+                false
+            end
+        else
+            false
+        end
+    end
+
+    def close_account
+        self.balance = 0
+        self.status = 'closed'
+    end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,60 @@
+require 'pry'
+
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+
+  @@transfer_history = []
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = 'pending'
+    @amount = amount
+  end
+
+  def valid?
+    @sender.valid?
+    @receiver.valid?
+  end
+
+  def self.transfer_history
+    @@transfer_history
+  end
+
+  def execute_transaction
+    previous_transfer = self.class.transfer_history.each do | history |
+      history == self
+    end
+
+    if previous_transfer.length > 0
+      return
+    end
+
+
+    if @sender.valid? == true && @receiver.valid? == true
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+      self.class.transfer_history << self
+    end
+
+    if @sender.valid? == false || @receiver.valid? == false
+      @status = 'rejected'
+      "Transaction rejected. Please check your account balance."
+    end
+
+
+  end
+
+  def reverse_transfer
+    executed_transfer = self.class.transfer_history.select do | transfer |
+      transfer.sender == @sender
+      transfer.receiver == @receiver
+    end
+    executed_transfer.each do | send_rec |
+      send_rec.sender.balance += send_rec.amount
+      send_rec.receiver.balance -= send_rec.amount
+      send_rec.status = "reversed"
+    end
+  end
 end
